9eae0ec745021cb166223bd004bdebe6c38653de
Brendan Boyle <brendan.boyle02@gmail.com>
2022-03-24 21:01:51 -0500

    Unnecessary period removed

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ export default function About() {
             <div className="container px-5 py-10 mx-auto flex items-center">
                 <div className="lg:flex-grow md:w-auto lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
                     <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-                        Welcome!.
+                        Welcome!
                         {//<br className="hdden lg:inline-block"/>I love to build amazing apps
                         }
                     </h1>
